3a05c38f9d12873dc794eb7a5bf3602ff38f4129
Rodrigo Rezende <rodrigo.rez@gmail.com>
2025-06-21 17:00:33 -0300

    Create main.js
    criação do main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+function gerarQRCode() {
+  fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?action=criarHash')
+    .then(res => res.json())
+    .then(data => {
+      const url = `https://seu-dominio.com/form.html?hash=${data.hash}`;
+      document.getElementById('qrcode-container').innerHTML = `
+        <img src="https://quickchart.io/qr?text=${url}&size=200x200" />
+      `;
+      iniciarContagemAutomatica();
+    });
+}
+
+function atualizarDashboard() {
+  fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?action=listarPresencas')
+    .then(res => res.json())
+    .then(data => {
+      const lista = document.getElementById('lista-presencas');
+      lista.innerHTML = '';
+      data.forEach(presenca => {
+        const li = document.createElement('li');
+        li.textContent = `${presenca.nome} (${presenca.ra})`;
+        lista.appendChild(li);
+      });
+    });
+}
+
+function iniciarContagemAutomatica() {
+  let tempo = 20;
+  const timerElement = document.getElementById('tempo-restante');
+  const interval = setInterval(() => {
+    tempo--;
+    timerElement.innerText = `Próximo QR Code em: ${tempo}s`;
+
+    if (tempo <= 0) {
+      clearInterval(interval);
+      gerarQRCode();
+    }
+  }, 1000);
+}
+
+setInterval(atualizarDashboard, 5000); // Atualiza dashboard a cada 5 segundos
